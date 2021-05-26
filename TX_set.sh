25ee4bca8620fcfa21a1c5b88f7a85a123dec490
lucas556 <56923561@qq.com>
2021-05-27 00:31:05 +0800

    Update TX_set.sh

--- TX_set.sh
+++ TX_set.sh
@@ -6,11 +6,12 @@ sudo apt install -y ntpdate jq
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 ntpdate time1.tencentyun.com
 
+sudo dpkg -i bee_0.5.3_amd64.deb
+
 mkdir -p /Users/sig/.bee
 
 sudo chown -R bee:bee /var/lib/bee
 sudo chown -R /Users/sig/.bee
-sudo dpkg -i bee_0.5.3_amd64.deb
 
 rm -rf /etc/bee/bee.yaml
 
