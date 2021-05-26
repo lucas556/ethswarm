64a4ed4a27102376dc220d53c15ca2fe793f4324
lucas556 <56923561@qq.com>
2021-05-26 22:57:07 +0800

    Update TX_set.sh

--- TX_set.sh
+++ TX_set.sh
@@ -6,14 +6,6 @@ sudo apt install -y ntpdate
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 ntpdate time1.tencentyun.com
 
-wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.12/bee-clef_0.4.12_amd64.deb
-sudo dpkg -i bee-clef_0.4.12_amd64.deb
-
-systemctl status bee-clef
-
-wget https://github.com/ethersphere/bee/releases/download/v0.6.1/bee_0.6.0_amd64.deb
-sudo dpkg -i bee_0.6.0_amd64.deb
-
 sudo dpkg -i bee_0.5.3_amd64.deb
 
 rm -rf /etc/bee/bee.yaml
